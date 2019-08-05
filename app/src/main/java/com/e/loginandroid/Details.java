ee0af3bbe47464e12b3ec5e376a2e7181c4342e5
willmont21 <willmont66@gmail.com>
2019-08-04 23:22:03 -0500

    Using SharedPreferences

---
+++ app/src/main/java/com/e/loginandroid/Details.java
@@ -0,0 +1,37 @@
+package com.e.loginandroid;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Details extends AppCompatActivity {
+
+    SharedPreferences prf;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        TextView result = (TextView)findViewById(R.id.resultView);
+        Button btnLogOut = (Button)findViewById(R.id.btnLogOut);
+        prf = getSharedPreferences("user_details",MODE_PRIVATE);
+        intent = new Intent(Details.this,MainActivity.class);
+        result.setText("Hello, "+prf.getString("username",null));
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = prf.edit();
+                editor.clear();
+                editor.commit();
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Logout Successful",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
